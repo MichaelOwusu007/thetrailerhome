849546722292629c4a930b33ce7e80953f75a4cd
MichaelOwusu <nanayawwiafe007@gmail.com>
2023-11-25 17:38:53 +0000

    thetrailerhome

--- src/row.jsx
+++ src/row.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import axios from './axios';
 import "./row.css";
 import YouTube from 'react-youtube';
 import movieTrailer from 'movie-trailer';
-
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
     const [movies, setMovies] = useState([]);
@@ -11,22 +9,28 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     const [selectedMovie, setSelectedMovie] = useState(null);
     const base_url = "http://image.tmdb.org/t/p/original/";
 
+    const fetchMoviesData = async () => {
+        try {
+            const timestamp = new Date().getTime(); // Get current timestamp
+            const response = await fetch(`https://api.themoviedb.org/3${fetchUrl}&api_key=13ed1ecfaf93ecd77dad082740d033a2&timestamp=${timestamp}`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setMovies(data.results.filter(movie => movie.poster_path && movie.backdrop_path));
+        } catch (error) {
+            console.error('Error fetching movies:', error);
+        }
+    };
 
     useEffect(() => {
-        async function fetchData() {
-
-            const request = await axios.get(fetchUrl);
-            setMovies(request.data.results.filter(movie => movie.poster_path && movie.backdrop_path));
-
-        }
-
-        fetchData();
+        fetchMoviesData();
     }, [fetchUrl]);
 
     const opts = {
         height: "500",
         width: "100%",
-        playerVars: { // https://developers.google.com/youtube/player_parameters
+        playerVars: {
             autoplay: 1,
         },
     };
@@ -43,17 +47,13 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
                     setTrailerUrl(urlParams.get("v"));
                     setSelectedMovie(movie);
                 } else {
-                    // Handle case when trailer is not found
                     console.log("Trailer not found for the selected movie.");
                 }
             } catch (error) {
                 console.error("Error fetching trailer: ", error);
             }
         }
-
-
     };
-
 
     return (
         <div className='row'>
@@ -71,25 +71,23 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
                             ) : (
                                 <img
                                     className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                                    src="/no_preview.jpg" // Path to your fallback image
+                                    src="/no_preview.jpg"
                                     alt="No Review"
                                 />
                             )}
                             <div className="row-content">
                                 <h2 className='row-title'>{movie?.title || movie?.name || movie?.original_name}</h2>
-
                             </div>
                         </div>
                     )
                 ))}
             </div>
 
-            {trailerUrl && setSelectedMovie && (
+            {trailerUrl && selectedMovie && (
                 <div className="modal-background fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-[rgba(0,0,0,0.6)] ">
                     <div className="modal-video w-[1200px] lg:h-[720px] relative bg-black mb-2 mt-2">
                         <YouTube videoId={trailerUrl} opts={opts} />
                         <div className="py-1 px-3 w-[70px] rounded bg-blue-500 right-[180px] top-[20px] absolute  font-bold cursor-pointer " onClick={() => { setTrailerUrl(""); document.body.classList.remove("modal-open"); }}>Close</div>
-                        {/* aditional movie data  */}
                         <div className='details-container px-20 py-5 righteous items-center hidden lg:flex '>
                             <div className='title-overview'>
                                 <h4 className='text-lg lg:text-2xl font-bold righteous'>{selectedMovie?.title || selectedMovie?.name || selectedMovie?.original_name}</h4>
@@ -101,8 +99,8 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
             )}
 
         </div>
-
     );
 }
 
 export default Row;
+

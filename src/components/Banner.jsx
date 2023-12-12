ae0f571a472b71398582e03b77b087421b42cff9
Mickerl_Codes <132717728+MichaelOwusu007@users.noreply.github.com>
2023-12-12 18:48:48 +0000

    Add files via upload
    thetrailerhome

---
+++ src/components/Banner.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import "./Banner.css";
+import requests from "../util/requests";
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
+function Banner() {
+    const [movies, setMovies] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [trailerUrl, setTrailerUrl] = useState("");
+
+    useEffect(() => {
+        const fetchMoviesData = async () => {
+            try {
+                const timestamp = new Date().getTime();
+                const response = await fetch(`https://api.themoviedb.org/3${requests.fetchTrending}&api_key=13ed1ecfaf93ecd77dad082740d033a2&timestamp=${timestamp}`);
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                const filteredMovies = data.results.filter(movie => movie.backdrop_path);
+                setMovies(filteredMovies);
+            } catch (error) {
+                console.error('Error fetching movies:', error);
+            }
+        };
+
+        fetchMoviesData();
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % movies.length);
+        }, 7000);
+
+        return () => clearInterval(interval);
+    }, [movies]);
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substring(0, n - 1) + '...' : string;
+    }
+
+    const currentMovie = movies[currentIndex];
+
+    if (movies.length === 0) {
+        return null;
+    }
+
+    const handleClick = async () => {
+        try {
+            const movieName = currentMovie?.title || currentMovie?.name || currentMovie?.original_name || "";
+            const url = await movieTrailer(movieName);
+            if (url) {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+                document.body.classList.add("modal-open");
+            } else {
+                console.log("Trailer not found for the selected movie.");
+            }
+        } catch (error) {
+            console.error("Error fetching trailer: ", error);
+        }
+    };
+
+    const handleMoreInfoClick = async () => {
+        try {
+            const movieName = currentMovie?.title || currentMovie?.name || currentMovie?.original_name || "";
+            const url = await movieTrailer(movieName);
+            if (url) {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get("v"));
+                document.body.classList.add("modal-open");
+            } else {
+                console.log("Trailer not found for the selected movie.");
+            }
+        } catch (error) {
+            console.error("Error fetching trailer: ", error);
+        }
+    };
+
+    const opts = {
+        height: "550",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
+    return (
+        <div>
+            <header className='banner overflow-hidden'
+                style={{
+                    backgroundImage: `url("https://image.tmdb.org/t/p/original/${currentMovie?.backdrop_path}")`,
+                    backgroundPosition: "center center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    transition: "background-image 0.6s ease-in-out"
+                }}
+            >
+                <div className="banner-content ml-[10px] md:ml-[50px] ">
+                    <h1 className='banner-title text-[1.2rem] sm:text-[3rem] righteous'>{currentMovie?.title || currentMovie?.name || currentMovie?.original_name}</h1>
+                    <div className="banner-buttons ">
+                        <button className='banner-button p-[10px] sm:p-[15px] sm:text-[16px] ' onClick={handleClick}>Play</button>
+                        <button className='banner-button  p-[10px] sm:p-[15px] sm:text-[16px]  ' onClick={handleMoreInfoClick} >More Info...</button>
+                    </div>
+                    <h3 className='banner-description text-[15px] sm:text-[18px] '>{truncate(currentMovie?.overview, 150)}</h3>
+                </div>
+                <div className="banner--fadeBottom" />
+            </header>
+
+            {trailerUrl && (
+                <div className="modal-background fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-[rgba(0,0,0,0.6)] ">
+                    <div className="modal-video w-[1200px]  relative bg-black mb-2 mt-2">
+                        <YouTube videoId={trailerUrl} opts={opts} />
+                        <div className="close-button absolute w-[70px] top-[20px] right-[180px] cursor-pointer text-white  bg-blue-500 rounded px-3 py-1  font-bold " onClick={() => { setTrailerUrl(""); document.body.classList.remove("modal-open"); }}>Close</div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Banner;
+
+
+
